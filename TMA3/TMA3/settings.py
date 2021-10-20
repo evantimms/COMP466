796353677a5dbc00503a12ae611f34516ef9a359
evantimms <timmsevan@gmail.com>
2021-10-20 17:55:45 -0400

    mend

--- TMA3/TMA3/settings.py
+++ TMA3/TMA3/settings.py
@@ -38,6 +38,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT ='/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'shared/'),
