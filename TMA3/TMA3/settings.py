eea06f30a5c1569084b7816dcbe9d479ff0fe272
evantimms <timmsevan@gmail.com>
2021-10-19 22:58:55 -0400

    cors attempt 1

--- TMA3/TMA3/settings.py
+++ TMA3/TMA3/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # CORS
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
